1a588f266225aee8e96f0105b706d872db56d957
Michael Pletcher <u1325975@notchpeak15.int.chpc.utah.edu>
2025-01-27 15:19:46 -0700

    Add new models and update script

---
+++ scripts/agl_funcs.py
@@ -0,0 +1,23 @@
+"""
+Michael Pletcher
+Created: 01/24/2024
+Edited: 
+
+##### Summary #####
+.py script containg function to 
+
+########### Function List ###########
+    calc_total_melting_energy() - 
+    calc_final_layer_melting_energy() - 
+    calc_layer_melting_energy() - 
+    calc_total_melting_energy_1d() - 
+    calc_final_layer_melting_energy_1d() - 
+    calc_layer_melting_energy_1d() - 
+
+
+
+"""
+
+
+
+
